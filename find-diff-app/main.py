55061e91278a8ac91b158b3bd1d2c95f93bb55ca
uni <yuni.todate@gmail.com>
2022-05-23 20:47:42 +0000

    add orb

--- find-diff-app/main.py
+++ find-diff-app/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from skimage import io, util
+import skimage
 
 
 def src_target(img):
@@ -11,6 +12,21 @@ def src_target(img):
     target = util.crop(img, ((0, 0), (w, 0), (0, 0)))
 
     return src, target
+
+
+def feat_orb(src, target, n_keypoints):
+    """
+    https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.ORB
+    detector_extractor1.keypoints[matches[:, 0]]
+    """
+    orb1 = skimage.feature.ORB(n_keypoints=n_keypoints)
+    orb2 = skimage.feature.ORB(n_keypoints=n_keypoints)
+    de1 = orb1.detect_and_extract(src)
+    de2 = orb1.detect_and_extract(target)
+    matches = skimage.feature.match_descriptor(
+        de1.descriptors, de2.descriptors)
+
+    return de1, de2, mathces
 
 
 def main():
