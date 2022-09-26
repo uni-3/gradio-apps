27f6afd3815487d21d96bf4a1d092c272b602c05
uni <yuni.todate@gmail.com>
2022-09-26 06:32:34 +0000

    whisper implement

--- whisper-app/app.py
+++ whisper-app/app.py
@@ -1,10 +1,9 @@
+import whisper
 import gradio as gr
 
 import os
 os.system("pip install git+https://github.com/openai/whisper.git")
-import gradio as gr
 
-import whisper
 
 model = whisper.load_model("small")
 
@@ -15,33 +14,36 @@ def inference(audio):
 
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    _, probs = model.detect_language(mel)
+    l, probs = model.detect_language(mel)
 
     options = whisper.DecodingOptions(fp16=False)
     result = whisper.decode(model, mel, options)
 
+    print(l, probs)
     print(result.text)
     return result.text
 
 
 title = "Whisper"
 
-description = "Whisper is a general-purpose speech recognition model. It is trained on a large dataset of diverse audio and is also a multi-task model that can perform multilingual speech recognition as well as speech translation and language identification."
-
 b = gr.Blocks()
 
 if __name__ == "__main__":
-    # demo = gr.Interface(fn=echo, inputs="text", outputs="text")
-    # demo = gr.Interface(upload_img, gr.Image(shape=(200, 200)), "image")
     with b:
-        gr.Markdown("input src/target img")
-        with gr.Row():
-            src = gr.Image()
-            target = gr.Image()
+        gr.Markdown("input audio")
+        with gr.Group():
+            with gr.Box():
+                with gr.Row():
+                    audio = gr.Audio(
+                        label="input audio",
+                        show_label=False,
+                        source="microphone",
+                        type="filepath"
+                    )
 
-        img_output = gr.Image(label="result")
-        img_button = gr.Button("show")
+                button = gr.Button("transcribe")
 
-        img_button.click(res, inputs=[src, target],
-                         outputs=img_output)
-    demo.launch()
+            text = gr.TextBox(show_label=False)
+            button.click(inference, inputs=[audio],
+                         outputs=[text])
+    b.launch()
