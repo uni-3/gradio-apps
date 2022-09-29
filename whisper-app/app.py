7c173b20a0a02cdcbe7bbbde4f9f599dffa71bd5
uni <yuni.todate@gmail.com>
2022-09-29 00:52:58 +0000

    implemtn

--- whisper-app/app.py
+++ whisper-app/app.py
@@ -5,7 +5,7 @@ import gradio as gr
 model = whisper.load_model("small")
 
 
-def inference(mic=None, audio_file=None):
+def inference(mic, audio_file, model, without_timestamp):
     if mic is not None:
         audio = whisper.load_audio(mic)
     elif audio_file is not None:
@@ -15,9 +15,11 @@ def inference(mic=None, audio_file=None):
     audio = whisper.pad_or_trim(audio)
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
-    options = whisper.DecodingOptions(fp16=False)
+    #_, probs = model.detect_language(mel)
+    #print(f"Detected language: {max(probs, key=probs.get)}")
+    options = whisper.DecodingOptions(
+        fp16=False, without_timestamps=without_timestamp,
+        language="ja")
     result = whisper.decode(model, mel, options)
 
     print(result.tokens)
@@ -29,12 +31,14 @@ def inference(mic=None, audio_file=None):
 title = "Whisper"
 
 avail_models = whisper.available_models()
+# 日本語のみ対応のため
+avail_models = [m for m in avail_models if m not in "en"]
 b = gr.Blocks()
 
 if __name__ == "__main__":
     with b:
         with gr.Column():
-            gr.Markdown("### input")
+            gr.Markdown("### input 日本語音声のみ")
             with gr.Row():
                 audio = gr.Audio(
                     label="input audio",
@@ -52,15 +56,14 @@ if __name__ == "__main__":
                     optional=True
                 )
 
-            model = gr.Checkboxgroup(avail_models, label="model")
+            model = gr.Checkbox(avail_models, label="model")
 
-            with gr.Accordion("settings", open=False):
-                with_timestamp = gr.Checkbox(
-                    label="with timestamp", value=True)
+            without_timestamp = gr.Checkbox(
+                label="with timestamp", value=True)
 
             button = gr.Button(label="transcribe")
 
         text = gr.Textbox(show_label=False, max_lines=10)
-        button.click(fn=inference, inputs=[audio, audio_file, model, language, with_timestamp],
+        button.click(fn=inference, inputs=[audio, audio_file, model, without_timestamp],
                      outputs=[text])
     b.launch()
